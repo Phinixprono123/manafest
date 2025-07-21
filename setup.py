e3abbdac877c45d38e78b5d7d29dd2354ed616e2
Phinixprono123 <alkamasudad3@gmail.com>
2025-07-21 11:30:12 +0600

    Update setup.py

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ setup(
     description="Manafest: multi-backend package manager",
     author="Alkama Sudad",
     packages=find_packages(),  # will find manafest + subpackages
-    install_requires=["rich", "psutil", "pygit2", "requests"],
+    install_requires=["rich", "psutil", "pygit2", "requests", "aiohttp"],
     entry_points={
         "console_scripts": [
             "manafest=manafest.cli:main",
